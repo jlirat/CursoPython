26210b9dc6fd866ffd974a6a5f0d4b0e795c4de7
tartaman <victor.ealpuche@prepafrayangelico.edu.mx>
2022-02-11 17:29:57 -0600

    Update 1009.py

--- VICTOR/Victor 1009/1009.py
+++ VICTOR/Victor 1009/1009.py
@@ -3,4 +3,4 @@ NAME = input()
 SALARY = float(input())
 SALES = float(input())
 BONUS = SALES *0.15
-print('TOTAL = $R {:.2f}'.format(SALARY + BONUS))
+print('TOTAL = R$ {:.2f}'.format(SALARY + BONUS))
