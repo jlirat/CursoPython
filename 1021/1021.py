2e6a951059d25254d261a21fac06ab115c23a500
Jose Luis Lirac <jl.lira@gmail.com>
2022-02-19 17:15:24 -0600

    ciclos y cadenas

---
+++ 1021/1021.py
@@ -0,0 +1,22 @@
+v = float(input())
+notas = [100.0, 50.0, 20.0, 10.0, 5.0, 2.0]
+coins = [1.0, 0.50, 0.25, 0.10, 0.05, 0.01]
+
+notes = {}
+moedas = {}
+for n in notas:
+    p1 = v // n
+    v = v - (p1 * n)
+    
+    notes[n] = p1
+for c in coins:
+    p1 = v // c
+    v = v - (p1 * c)
+
+    moedas[str(c)] = p1
+print('NOTAS:')
+for n in notas:
+    print('{0:.0f} nota(s) de R$ {1:.2f}'.format(notes[n], n))
+print('MOEDAS:')
+for c in coins:
+    print('{0:.0f} nota(s) de R$ {1:.2f}'.format(moedas[str(c)], c))
--- otros/tree.py
+++ otros/tree.py
@@ -25,20 +25,59 @@
 #...
 # 255
 
-x = 2
-y = 2.0
-z = False
-w = "Hello"
-a = "World"
-b = "!"
-c = 4
-d = 1
-resultado  = x and z # or ||
-resultado2 = x + y
-resultado3 = x & c
+# x = 2
+# y = 2.0
+# z = False
+# w = "Hello"
+# a = "World"
+# b = "!"
+# c = 4
+# d = 1
+# resultado  = x and z # or ||
+# resultado2 = x + y
+# resultado3 = x & c
 
-resultado4 = d << 5
+# resultado4 = d << 5
 
-# for x in range(2,2,2,2,2):
-#     d*=x    
-print(resultado4)
+# # for x in range(2,2,2,2,2):
+# #     d*=x    
+# print(resultado4)
+
+# Hacer un programa que determine si un número es par o impar
+# 111001
+# 000001
+# 000001 = 1
+# 110000
+# 000011
+#=000000 = 0
+
+# num = int(input())
+
+# if (num & 1)==0:
+#     print('par')
+# else:
+#     print('impar')
+
+# cad = "cadena"
+# otra_cadena = 'otra_cadena'
+# esta_cadena = "\"cadena\" y \"otra_cadena\" \t\r\n"
+# numero1 = str(10)
+# numero2 = str(11)
+# especial = f"10 + 11 = {10 + 11}"
+# longitud = len(especial)
+# print(cad)
+# print(otra_cadena)
+# print(numero1 + numero2)
+# print(longitud)
+# print(especial)
+# print(esta_cadena)
+# print(esta_cadena[3:5].upper())
+# print(esta_cadena[3:5].lower())
+
+s = input()
+new_s = ''
+for c in s:
+    new_c = ord(c) + 3
+    print("\{}".format(new_c))
+    new_s+=chr(ord(c)+3)
+print(new_s)
