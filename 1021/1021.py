2802789866f5f72e45590742db9fed41c187863b
Jose Luis Lirac <jl.lira@gmail.com>
2022-02-24 19:45:55 -0600

    round

--- 1021/1021.py
+++ 1021/1021.py
@@ -1,24 +1,32 @@
 v = float(input())
-notas = [100.0, 50.0, 20.0, 10.0, 5.0, 2.0]
-coins = [1.0, 0.50, 0.25, 0.10, 0.05, 0.01]
+# coins = [1.0, 0.50, 0.25, 0.10, 0.05, 0.01]
+# notas = [100.0, 50.0, 20.0, 10.0, 5.0, 2.0]
+notas = [100.0, 50.0, 20.0, 10.0, 5.0, 2.0, 1.0, 0.50, 0.25, 0.10, 0.05, 0.01]
 
-notes = {}
-moedas = {}
+# notes = {}
+# moedas = {}
+
 if (0.00 <= v <= 1000000.00):
-    for n in notas:
-        p1 = v / n
-        # v = v - (p1 * n)
-        v = v % n
-        notes[n] = int(p1)
-    for c in coins:
-        p1 = v / c
-        # v = v - (p1 * c)
-        v = v % c
-
-        moedas[str(c)] = int(p1)
     print('NOTAS:')
+    band = False
     for n in notas:
-        print('{0:.0f} nota(s) de R$ {1:.2f}'.format(notes[n], n))
-    print('MOEDAS:')
-    for c in coins:
-        print('{0:.0f} moeda(s) de R$ {1:.2f}'.format(moedas[str(c)], c))
+        if n < 2 and band == False:
+            print('MOEDAS:')
+            band = True
+        p1 = v // n
+        v = round(v % n, 2)
+        
+        # notes[n] = int(p1)
+        # p1 = int(p1)
+        print('{0:.0f} nota(s) de R$ {1:.2f}'.format(p1, n))
+    # for c in coins:
+    #     p1 = v / c
+    #     v = v % c
+    #     print(v)
+    #     # moedas[str(c)] = int(p1)
+    # print('NOTAS:')
+    # for n in notas:
+    #     print('{0:.0f} nota(s) de R$ {1:.2f}'.format(notes[n], n))
+    # print('MOEDAS:')
+    # for c in coins:
+    #     print('{0:.0f} moeda(s) de R$ {1:.2f}'.format(moedas[str(c)], c))
