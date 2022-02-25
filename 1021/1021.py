22b04005d79a74bee06d1698bf4a27b2aaf41e31
Jose Luis Lirac <jl.lira@gmail.com>
2022-02-24 21:49:41 -0600

    short 1021

--- 1021/1021.py
+++ 1021/1021.py
@@ -13,12 +13,18 @@ if (0.00 <= v <= 1000000.00):
         if n < 2 and band == False:
             print('MOEDAS:')
             band = True
-        p1 = v // n
-        v = round(v % n, 2)
+        p1 = v / n
+        v = v % n
+        
+        if v < 0.01:
+            v = round(v,2)
         
         # notes[n] = int(p1)
-        # p1 = int(p1)
-        print('{0:.0f} nota(s) de R$ {1:.2f}'.format(p1, n))
+        p1 = int(p1)
+        if not band:
+            print('{0:.0f} nota(s) de R$ {1:.2f}'.format(p1, n))
+        else:
+            print('{0:.0f} moeda(s) de R$ {1:.2f}'.format(p1, n))
     # for c in coins:
     #     p1 = v / c
     #     v = v % c
